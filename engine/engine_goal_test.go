e9dffb949879a89a080693fa16bfe342b643d78e
Frode Bjerke <frode.bjerke@gmail.com>
2015-08-26 16:26:36 +0200

    add donut score test test

--- engine/engine_goal_test.go
+++ engine/engine_goal_test.go
@@ -32,6 +32,33 @@ func TestBlueGoalOneNil(t *testing.T) {
     }
 }
 
+func TestBlueDonut(t *testing.T) {
+    tablename := "skuret"
+    engine := matchWithEngine(tablename)
+
+
+    updates := engine.GetTableUpdateChannel(tablename)
+    <- updates
+
+    for i := 0; i < 10; i++ {
+        bluegoalevent, err := events.NewGoal("blue", 1+i, 1+i, time.Now())
+        if err != nil {
+            t.Errorf("did not expect error when creating goal")
+        }
+        engine.GoalEvent(tablename, bluegoalevent)
+
+        blueGoalUpdate := <- updates
+
+        if blueGoalUpdate.Blue.Score != i+1 {
+            t.Errorf("Blue goal. Expect blue score to be %d got %d", i+1, blueGoalUpdate.Blue.Score)
+        }
+
+        if blueGoalUpdate.White.Score != 0 {
+            t.Errorf("Blue goal. Expect white score to be 0 got %d", blueGoalUpdate.White.Score)
+        }
+    }
+}
+
 func matchWithEngine(tablename string) *Engine {
     newmatchevent := &events.NewMatchEvent{ uuid.NewRandom(), uuid.NewRandom(), uuid.NewRandom(), uuid.NewRandom(), time.Now() }
     engine := CreateEngine()
